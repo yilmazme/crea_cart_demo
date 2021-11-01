3a00f15f8e5c2cca8a7550388fcf0d3915a096b7
unknown <10myilmaz@gmail.com>
2021-11-01 21:48:28 +0300

    jwt and _redirects added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,18 @@
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, of, throwError, BehaviorSubject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+
+  userInfo = new BehaviorSubject(null);
+  jwtHelper = new JwtHelperService();
+
   constructor(private router: Router) {}
 
   setToken(token: string): void {
@@ -18,6 +24,7 @@ export class AuthService {
   }
 
   isLoggedIn() {
+    // tek kullanıcı için decode etmeye gerek yok, decodda sadece user, iat filan gelecek
     return this.getToken() !== null;
   }
 
@@ -26,11 +33,21 @@ export class AuthService {
     this.router.navigate(['login']);
   }
 
-  login(usename:string, password:string ): Observable<any> {
-    if (usename === 'user' && password === 'user123') {
-      this.setToken('JKHKDSJDH876868jjgjgjg76757jgJG');
-      return of({ name: 'Mehmet Yılmaz', usename: 'user' });
-    }
-    return throwError(new Error('username or password incorrect!'));
+  login(username:string, password:string ): Observable<any> {
+    if(username==="user" && password==="user12345"){
+       const sampleJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJzdWIiOjIsImlhdCI6MTYzNTYzOTc0OSwiZXhwIjoxNjM3MzQ5NzQ5fQ.0kneNoDovsi5QA9PrfgwXn0lFixgeArHeXPYYRtOipc";
+     
+       return of(sampleJWT).pipe(
+       map((token) => {
+      if(!token){
+        return false;
+      }
+      localStorage.setItem("token", token);
+      const decodedUser = this.jwtHelper.decodeToken(token);
+      this.userInfo.next(decodedUser);
+      return true;
+       }));
+      }
+      return throwError(new Error('username or password incorrect!'));
   }
 }
